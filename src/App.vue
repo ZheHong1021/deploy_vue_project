f4eab64dd23045f8b1dfad7b5d4905b27326812b
ZheHong1021 <zhehome1021@gmail.com>
2024-05-26 18:48:49 +0800

    feat: 建立API Configure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <v-app>
-
     <v-main>
+      <div class="d-flex gap-4">
+        <router-link v-for="route in routes" :key="route['name']"
+          :to="route['path']">
+          {{ route['name'] }}
+        </router-link>
+      </div>
+
+
       <router-view/>
     </v-main>
   </v-app>
@@ -13,7 +20,19 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    routes: [
+      {name: 'HomeView', path: '/'},
+      {name: 'AboutView', path: '/about'},
+      {name: 'WebSocket', path: '/web-socket'},
+    ]
+
   }),
 };
 </script>
+
+
+<style lang="scss" scoped>
+ @import url('assets/scss/style.scss');
+
+
+</style>
