2643baa53d5c7c9e9c0acd39ed71b0f5ce51a6d2
ZheHong1021 <zhehome1021@gmail.com>
2024-06-19 23:54:16 +0800

    feat: 安裝jwt-decode進行第一步驗證Token(不然驗證API會一直呼叫)

--- src/api/server.js
+++ src/api/server.js
@@ -11,16 +11,37 @@ const server = axios.create({
   }
 });
 
+
+
+import { jwtDecode } from "jwt-decode";
+
+// JWT 檢查是否過期
+function isTokenExpired(token) {
+  try {
+    const decoded = jwtDecode(token);
+    const currentTime = Date.now() / 1000;
+    return decoded.exp < currentTime;
+  } catch (e) {
+    return true;
+  }
+}
+
+
 server.interceptors.request.use(async (config) => {
   // 當前TOKEN(在 localStorage)
   let accessToken = localStorage.getItem('accessToken') || null;
   
   // 如果有 TOKEN => 需要去驗證
   if (accessToken) {
-    await store.dispatch("auth/verifyAccessToken")
+    // 驗證該 accessToken是否過期
+    if (isTokenExpired(accessToken)) {
+      // 透過 VueX去檢查是否驗證成功
+      await store.dispatch("auth/verifyAccessToken")
 
-    // 可能會刷新 TOKEN，因此要重新捕捉
-    accessToken = localStorage.getItem('accessToken')
+      // 可能會刷新 TOKEN，因此要重新捕捉
+      accessToken = localStorage.getItem('accessToken')
+    }
+
     config.headers.Authorization = `Bearer ${accessToken}`;
   }
   return config;
