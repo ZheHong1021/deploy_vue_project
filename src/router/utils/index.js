fb3233dda41facc86996c2091268194de3a12795
ZheHong1021 <zhehome1021@gmail.com>
2024-06-21 01:42:54 +0800

    fix: 調整router中讀取vuex getters的方式

--- src/router/utils/index.js
+++ src/router/utils/index.js
@@ -2,7 +2,7 @@ import store from '@/store';
 
 const checkAuthIsLoggedIn = (to) => {
     // [是否登入]: 不存在會返回 undefined
-    const isLoggedIn = store.getters.auth?.isLoggedIn
+    const isLoggedIn = store.getters['auth/isLoggedIn']
 
     // 確認該route中的meta有無登入驗證的需求
     const metaIncludeRequireAuth = to.matched.some((route) => route['meta']['requireAuth'])
