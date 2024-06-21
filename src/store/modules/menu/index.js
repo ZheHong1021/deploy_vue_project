fa00bceee1c70dc63eaab09e04936c6144c65355
ZheHong1021 <zhehome1021@gmail.com>
2024-06-21 17:00:14 +0800

    feat: 添加CONST MENU(預設路由)至Sidebar

--- src/store/modules/menu/index.js
+++ src/store/modules/menu/index.js
@@ -2,23 +2,39 @@ import { MenuService } from '@/api/services'
 import { generateRoutes } from '@/router/utils';
 import router, { resetRouter } from "@/router"
 
+// 將vue-router的路由轉成能讓Sidebar讀取
+const tranformRouterToSidebar = (route) => {
+  return {
+    name: route['name'],
+    path: route['path'],
+    component: route['component'],
+    title: route['meta']['title'],
+    icon: route['meta']['icon'],
+    is_menu: route['meta']['is_menu'],
+  }
+}
+
 // 預設的前端路由
 const CONST_ROUTES = router.options.routes
-const CONST_MENU_ROUTES = CONST_ROUTES.filter(route => route['meta']['is_menu'])
-console.log(CONST_MENU_ROUTES);
+
+// 只需要菜單路由
+const CONST_MENU_ROUTES = CONST_ROUTES
+                          .filter(route => route['meta']['is_menu'])
+                          .map(menu => tranformRouterToSidebar(menu))
 
 export default {
   namespaced: true,
   state: {
     // 菜單 (預設所需)
-    menus: [], 
+    menus: CONST_MENU_ROUTES, // 預設有CONST的菜單路由
   },
   getters: {
   },
   mutations: {
     // 設定菜單列
     async setMenus(state, payload){
-      state.menus = payload
+      // 菜單路由
+      state.menus = [...CONST_MENU_ROUTES, ...payload]
       
       // 添加進去前，先清除一次
       await resetRouter()
@@ -29,7 +45,8 @@ export default {
     
     // 刪除菜單列
     clearMenus(state, payload){
-      state.menus = []
+      // 重新定義
+      state.menus = CONST_MENU_ROUTES
 
       // 登出後清空路由
       resetRouter()
