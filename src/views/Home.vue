d82ea8b5e96778e38a00e0e471908569f2e90f41
ZheHong1021 <zhehome1021@gmail.com>
2024-06-22 04:08:15 +0800

    feat: 製作測試版

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,30 +2,38 @@
   <v-container>
     <v-row>
       <v-col cols="12">
-        12312
+        <v-btn @click="dialog = true">新增數據</v-btn>
+        <CustomDialog v-model="dialog">
+          <template v-slot:body>
+            <div>
+              測試內容
+            </div>
+          </template>
+        </CustomDialog>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import { mapGetters, mapState } from 'vuex';
+import CustomDialog from "@/components/utils/CustomDialog.vue";
 export default {
+  components: {
+    CustomDialog,
+  },
   data() {
     return {
+      dialog: false,
     };
   },
-  async mounted() {
-  },
 
-  computed: {
-    ...mapState('menu', ['menus']),
-    ...mapGetters('auth', ['isLoggedIn']),
-  },
+  computed: {},
 
+  async mounted() {},
 
-  methods: {
-
-  },
+  methods: {},
 };
 </script>
+
+<style>
+</style>
