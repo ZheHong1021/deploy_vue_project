a0d7a45df5a24b5210866282fc1fa5d0782452f1
zhe-hong-Lin <58475836+ZheHong1021@users.noreply.github.com>
2024-06-22 11:46:42 +0800

    Feat/custom dialog (#14)
    * feat: 製作測試版
    
    * feat: 添加監聽上下頁觸發時關閉Dialog功能

--- src/components/utils/CustomDialog.vue
+++ src/components/utils/CustomDialog.vue
@@ -1,15 +1,27 @@
 <template>
-  <v-dialog v-model="dialog" fullscreen persistent @keydown.esc="closeDialog">
+
+  <v-dialog 
+    v-model="dialog"
+    :width="dialogWidth"
+    persistent>
+
     <v-card>
+      <!-- 頂部區域 -->
       <v-toolbar dark color="primary">
+        <!-- 關閉按鈕 -->
         <v-btn icon dark @click="closeDialog">
-          <v-icon>mdi-close</v-icon>
+          <v-icon>mdi-close-thick</v-icon>
         </v-btn>
-        <v-toolbar-title>新增數據</v-toolbar-title>
+
+        <!-- 標題 -->
+        <v-toolbar-title>
+          {{ title }}
+        </v-toolbar-title>
       </v-toolbar>
 
         <!-- 內容區域 -->
-      <v-card-text>
+      <v-card-text class="py-4 black--text">
+
         <slot name="body"></slot>
       </v-card-text>
 
@@ -20,7 +32,11 @@
 <script>
 export default {
   props: {
-    value: Boolean,
+
+    value: { // Parent提供的Dialog
+      type: Boolean,
+      default: false,
+    },
     color: { // title背景顏色
         type: String,
         default: "primary",
@@ -33,33 +49,107 @@ export default {
   data() {
     return {
       dialog: false,
+
+      originPath: null,
     };
   },
-  watch: {
-    value(val) {
-      this.dialog = val;
-    },
-    dialog(val) {
-      this.$emit('input', val);
+
+  computed:{
+    dialogWidth(){
+      const { rwd_name } = this
+      const width = {
+        'xs': '95%',
+        'sm': '80%',
+        'md': '70%',
+        'lg': '60%',
+        'xl': '50%',
+      }
+
+      return width[rwd_name]
     }
   },
-  mounted() {
-    window.addEventListener('popstate', this.closeDialog);
-  },
-  beforeDestroy() {
-    window.removeEventListener('popstate', this.closeDialog);
-  },
-  methods: {
-    closeDialog() {
-      this.dialog = false;
+  
+  watch: {
+    // Parent有trigger時
+    value(val) {
+      this.dialog = val;
+
+      if(val){ // 打開時
+        // 原始頁面的Path
+        this.originPath = window.location.hash
+
+        // Path改為 #dialog
+        history.pushState(
+          {dialogOpen: true }, // stateObj
+          '', // title
+          `${this.originPath}#dialog` // [, url]
+        );
+
+        // 監聽觸發上下頁時
+        window.addEventListener('popstate', this.handlePopState);
+      }
+      else{ // 關閉時
+        window.removeEventListener('popstate', this.handlePopState);
+      }
+
     },
+
+    // 如果有進行關閉 => 會回傳給 Parent做同步
+    dialog(val) {
+      this.$emit('input', val); // 通常都是關閉
+    }
+  },
+
+  mounted() {
+
+    // 如果之前有重新整理 => #dialog(還會存在) => 但這樣會累積超多#dialog#dialog
+    if (window.location.hash === '#dialog') {
+      // 要刪除
+      this.restoreOriginalHash();
+    }
+  },
+
+  beforeDestroy() { // 離開時也會關閉
+    window.removeEventListener('popstate', this.handlePopState);
+  },
+
+  methods: {
+    // 關閉Dialog
+    closeDialog() {
+      this.dialog = false; // 關閉
+      history.back(); // 調用後則會回到正常的URL
+    },
+
+
+    // 處理返回按鈕事件
+    handlePopState(event) {
+      // 判斷hash是否有 #dialog
+      if (window.location.hash === '#dialog') {
+        this.dialog = true; // 有 #hash: 代表開啟Dialog中
+      } 
+      else {
+        this.dialog = false;
+      }
+    },
+
+
+    // 回復最一開始的樣子
+    restoreOriginalHash() {
+      const { pathname, search } = window.location
+      // pathname: /f
+      // search: ?record_date=2024-06-06
+      const url = pathname + search
+
+      // 恢復
+      history.replaceState(null, '', url);
+    }
+   
+
   }
 };
 </script>
 
 <style scoped>
-.v-dialog--fullscreen {
-  max-height: 100%;
-  max-width: 100%;
-}
+
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,7 +29,10 @@ export default {
 
   computed: {},
 
-  async mounted() {},
+
+  async mounted() {
+  },
+
 
   methods: {},
 };
