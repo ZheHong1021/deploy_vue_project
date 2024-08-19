6c7a64f38fde17e02402b731946ba4a59e7c2d72
ZheHong1021 <zhehome1021@gmail.com>
2024-08-19 19:05:38 +0800

    feat: 客製化搜尋Box

---
+++ src/components/utils/CustomSearchBox.vue
@@ -0,0 +1,51 @@
+<template> 
+    <v-text-field
+        :value="value"
+        outlined
+        :background-color="backgroundColor"
+        :label="label"
+        :placeholder="placeholder"
+        @input="$emit('input', $event)"
+        prepend-inner-icon="mdi-magnify"
+        :clearable="clearable" clear-icon="mdi-close-circle"
+        hide-details>
+
+    </v-text-field>
+</template>
+
+<script>
+export default {
+    name: "CustomSearchBox",
+    props: {
+        value: {
+            type: String,
+            default: null,
+        },
+        backgroundColor: {
+            type: String,
+            default: "white",
+        },
+        label: {
+            type: String,
+            default: "搜尋",
+        },
+        placeholder: {
+            type: String,
+            default: "請搜尋文字",
+        },
+        clearable: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    data(){
+        return {
+
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
